e577ab0fbdc52c8c868be4f5cd04230984119445
unknown <josh@jlbworks.com>
2018-09-18 15:34:03 -0500

    Added navigation and media. Also set variables

--- themes/JLBTheme/header.php
+++ themes/JLBTheme/header.php
@@ -6,6 +6,7 @@
     <link rel="shortcut icon" type="image/x-icon" href="<?php echo home_url(); ?>">
     <link rel="apple-touch-icon" href="<?php echo home_url(); ?>"/>
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-oi8o31xSQq8S0RpBcb4FaLB8LJi9AT8oIdmS1QldR8Ui7KUQjNAnDlJjp55Ba8FG" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,300i,400,400i,700,700i" rel="stylesheet">
     <script defer src="https://pro.fontawesome.com/releases/v5.0.13/js/all.js" integrity="sha384-d84LGg2pm9KhR4mCAs3N29GQ4OYNy+K+FBHX8WhimHpPm86c839++MDABegrZ3gn" crossorigin="anonymous"></script>
     <?php wp_head(); ?>
   </head>
@@ -14,16 +15,13 @@
       <div class="header">
         <div class="header-content">
           <a class="logo-container" href="<?php echo home_url(); ?>">
-            <div class="logo" style="background-image: url('#logo');"></div>
+            <div class="logo" style="background-image: url(<?=get_field('header_logo', 'option'); ?>);"></div>
           </a>
         </div> <!-- Header Content & Logo -->
-        <nav class="header-nav">
-          <?php wp_nav_menu( array( 'menu' => 'Header Menu' ) ); ?>
-          <button type="button" name="search"><a href="<?php echo home_url(); ?>"><i class="fas fa-search"></i></a></button>
-        </nav> <!-- Header Navigation -->
 
         <!-- Button trigger modal -->
         <div class="mobile-button">
+          <p>Menu</p>
           <div class="button-container">
             <div class="bar1 bar"></div>
             <div class="bar2 bar"></div>
@@ -35,7 +33,7 @@
       <!-- Mobile Menu -->
       <div class="mobile-menu" id="mobile-menu">
         <nav class="menu-container">
-          <?php wp_nav_menu( array( 'menu' => 'Header Menu' ) ); ?>
+          <?php wp_nav_menu( array( 'menu' => 'main-menu' ) ); ?>
         </nav>
       </div>
 
