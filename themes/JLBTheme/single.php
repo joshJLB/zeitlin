51adf7e72fbe56ab6ae2fc8b889481cd14d91dd8
unknown <josh@jlbworks.com>
2018-09-20 12:06:33 -0500

    Added single blog and mobilized

--- themes/JLBTheme/single.php
+++ themes/JLBTheme/single.php
@@ -13,7 +13,17 @@ get_header(); ?>
       <?php while ( have_posts() ): the_post(); ?>
 
         <!-- Content Layout Here -->
-        <?php the_content(); ?>
+        <div class="single-blog-container">
+          <img src="<?=the_post_thumbnail_url(); ?>" alt="">
+          <div class="single-blog-content">
+            <div class="date-wrapper">
+              <h3 class="single-blog-date"><?=the_date(); ?></h3>
+            </div>
+            <?php the_content(); ?>
+            <h4>-<?php the_author(); ?></h4>
+          </div>
+        </div>
+
 
     <?php endwhile; wp_reset_postdata(); endif; ?>
   </div>
