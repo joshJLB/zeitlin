60f749a6a925d907ce9eb141bdad921f1e518455
unknown <josh@jlbworks.com>
2018-09-24 12:52:44 -0500

    Added section two on community page

--- themes/JLBTheme/single-community.php
+++ themes/JLBTheme/single-community.php
@@ -15,6 +15,27 @@ get_header(); ?>
         </div>
     </section>
 
+    <section class="two">
+        <div class="two-container">
+            
+                <?php if ( have_rows('two_repeater') ): ?>
+                <?php while ( have_rows('two_repeater') ): the_row(); ?>
+                    <div class="two-links-wrapper">
+                        <div class="two-links" style="background-image: url(<?=get_sub_field('image'); ?>);">
+                            <div class="overlay3"></div>
+                            <a href="<?=get_sub_field('link_url'); ?>">
+                                <div class="two-links-inner">
+                                    <?=get_sub_field('link_text'); ?>
+                                </div>
+                            </a>
+                        </div>  
+                    </div>
+                <?php endwhile; ?>
+                <?php endif; ?>
+            
+        </div>
+    </section>
+
 </main>
 
 <?php get_footer();
