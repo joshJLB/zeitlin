cf046cf553a0e2fb008136c2e5f83c57f73e1112
unknown <josh@jlbworks.com>
2018-09-25 11:19:57 -0500

    Updated child headers for different pages.

--- themes/JLBTheme/components/header/child-header.php
+++ themes/JLBTheme/components/header/child-header.php
@@ -9,15 +9,32 @@
 <?php
   $c = get_field('child_header');
   $h = get_field('child_header','option');
+  $j = get_field('child_header_two', 'option');
 ?>
 
-<div class="child-header" <?php if ($c): ?>style="background-image: url('<?php echo $c; ?>');"<?php else: ?>style="background-image: url('<?php echo $h; ?>');"<?php endif; ?>>
-  <?php if (is_home()): ?>
-    <h1 class="title">Blog</h1>
-  <?php elseif (is_single()): ?>
+
+<?php if (is_home()): ?>
+  <div class="child-header" <?php if ($c): ?>style="background-image: url('<?php echo $c; ?>');"<?php else: ?>style="background-image: url('<?php echo $h; ?>');"<?php endif; ?>>
     <h1 class="title"><?php echo get_the_title(); ?></h1>
-    <!-- Anything else for single page child header here -->
-  <?php else: ?>
+  </div>
+<?php elseif (is_page('communities')): ?>
+  <div class="child-header" <?php if ($c): ?>style="background-image: url('<?php echo $c; ?>');"<?php else: ?>style="background-image: url('<?php echo $h; ?>');"<?php endif; ?>>
     <h1 class="title"><?php echo get_the_title(); ?></h1>
-  <?php endif; ?>
-</div>
+  </div>
+<?php elseif (is_single()): ?>
+  <div class="child-header-two" style="background-image: url(<?=$j; ?>);">
+    <!-- <div class="child-header-contact">
+      <div class="child-header-image">
+        <img src="" alt="">
+      </div>
+    </div> -->
+    <div class="child-header-title">
+      <h1 class="title"><?php echo get_the_title(); ?></h1>
+      <p><?=get_field('header_text'); ?></p>
+    </div>
+  </div>
+<?php else: ?>
+  <div class="child-header" <?php if ($c): ?>style="background-image: url('<?php echo $c; ?>');"<?php else: ?>style="background-image: url('<?php echo $h; ?>');"<?php endif; ?>>
+    <h1 class="title"><?php echo get_the_title(); ?></h1>
+  </div>
+<?php endif; ?>
