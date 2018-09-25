f20e2998524a7bbf361b906304da081bf3b66fe1
unknown <josh@jlbworks.com>
2018-09-25 17:00:16 -0500

    Added info to child headers.

--- themes/JLBTheme/components/header/child-header.php
+++ themes/JLBTheme/components/header/child-header.php
@@ -13,21 +13,28 @@
 ?>
 
 
-<?php if (is_home()): ?>
+<?php if (is_home()): ?> 
   <div class="child-header" <?php if ($c): ?>style="background-image: url('<?php echo $c; ?>');"<?php else: ?>style="background-image: url('<?php echo $h; ?>');"<?php endif; ?>>
     <h1 class="title"><?php echo get_the_title(); ?></h1>
   </div>
 <?php elseif (is_page('communities')): ?>
   <div class="child-header" <?php if ($c): ?>style="background-image: url('<?php echo $c; ?>');"<?php else: ?>style="background-image: url('<?php echo $h; ?>');"<?php endif; ?>>
+    <div class="child-header-contact">
+      <div class="child-header-image">
+        <img src="<?=get_field('avatar', 'option'); ?>" alt="">
+      </div>
+      <p>Pat Patterson</p>
+    </div>
     <h1 class="title"><?php echo get_the_title(); ?></h1>
   </div>
 <?php elseif (is_single()): ?>
   <div class="child-header-two" style="background-image: url(<?=$j; ?>);">
-    <!-- <div class="child-header-contact">
+    <div class="child-header-contact">
       <div class="child-header-image">
-        <img src="" alt="">
+        <img src="<?=get_field('avatar', 'option'); ?>" alt="">
       </div>
-    </div> -->
+      <p>Pat Patterson</p>
+    </div>
     <div class="child-header-title">
       <h1 class="title"><?php echo get_the_title(); ?></h1>
       <p><?=get_field('header_text'); ?></p>
@@ -35,6 +42,12 @@
   </div>
 <?php else: ?>
   <div class="child-header" <?php if ($c): ?>style="background-image: url('<?php echo $c; ?>');"<?php else: ?>style="background-image: url('<?php echo $h; ?>');"<?php endif; ?>>
+    <div class="child-header-contact">
+      <div class="child-header-image">
+        <img src="<?=get_field('avatar', 'option'); ?>" alt="">
+      </div>
+      <p>Pat Patterson</p>
+    </div>
     <h1 class="title"><?php echo get_the_title(); ?></h1>
   </div>
 <?php endif; ?>
